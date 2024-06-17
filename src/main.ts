13e97ce260437bccbabae7c6992f1e8efccad500
Miguel Mendoza <miguel@zandome.com>
2024-06-17 01:48:52 -0400

    chore: implement run scripts with Bun

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import { AppModule } from '@/app/app.module'
 import { NestFactory } from '@nestjs/core'
 import {
   FastifyAdapter,
-  NestFastifyApplication,
+  type NestFastifyApplication,
 } from '@nestjs/platform-fastify'
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   )
-  await app.listen(3000)
+  await app.listen(Bun.env.PORT)
 }
 bootstrap()
